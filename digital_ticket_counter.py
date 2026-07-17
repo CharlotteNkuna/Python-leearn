371af36d5eea1aeb5e66e174adc3a397ccef9aa9
Charlotte <cnkuna920@gmail.com>
2026-07-17 09:34:57 +0200

    added lesson 2

---
+++ digital_ticket_counter.py
@@ -0,0 +1,8 @@
+print(f"Welcome to Charlotte's digital ticket counter!")
+print("Please enter the following details to generate your ticket.")
+
+name = input(f"Enter your name: ")
+surname =input(f"Enter your surname: ")
+artist = input(f"Which artist do you wnat to see?: ")
+
+print(f"Hey {name} {surname}, your ticket to see {artist} has been booked successfully. Enjoy the show!")
