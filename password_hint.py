a3a52b212fef448a406e9513ce215d14d79af6ff
Charlotte <cnkuna920@gmail.com>
2026-07-17 13:42:36 +0200

    added lesson2

--- password_hint.py
+++ password_hint.py
@@ -1,10 +1,10 @@
 #Manipulating strings
 
 password = input("Enter your password: ")
-password_space = password.strip()
+password = password.strip()
 
 #Grab the very first letter and the very last letter of the password using string indexing.
-first_letter = password_space[0]
-last_letter = password_space[-1]
+first_letter = password[0]
+last_letter = password[-1]
 
 print(f"Your password starts with: {first_letter} and ends with: {last_letter}")
